5e85570b9d5685c5d52ac4acb0df2de9e91396c4
Kevin Steven Arias Duarte <kariasduarte@gmail.com>
2024-02-15 14:55:23 -0500

    enrutamiento con link

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Navbar } from "../components/Navbar/Navbar";
 
 function About(){
     return(
         <>
+            <Navbar />
             <h1>Esta es otra paginas</h1>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Navbar } from "../components/Navbar/Navbar";
 
 function Home(){
     return(
         <>
+            <Navbar />
             <h1>Hola</h1>
         </>
     )
